fc113a485307fbaa9f4f352e8f917582f889489e
Spike <53007585+Wang4XD@users.noreply.github.com>
2025-04-20 16:43:43 +0800

    Delete file_utils.py

--- file_utils.py
+++
@@ -1,9 +0,0 @@
-import os
-from config import Config
-
-def save_uploaded_file(uploaded_file):
-    """保存上传文件"""
-    save_path = os.path.join(Config.UPLOAD_DIR, uploaded_file.name)
-    with open(save_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    return save_path
